89834982e3d0a2378f6047a5707569883a49876a
Tomaz Rodrigues <tomazrodrigues@Tomazs-Air.fios-router.home>
2025-02-05 16:12:21 -0500

    Added code to construct plot 1 and included plot1.PNG Code assumes dataSource is in local directory. May change this later(?)

---
+++ plot1.R
@@ -0,0 +1,27 @@
+mp1 <- function(){
+  
+  ## This first line will likely take a few seconds. Be patient!
+  NEI <- readRDS("summarySCC_PM25.rds")
+  SCC <- readRDS("Source_Classification_Code.rds")
+  
+  #subsetting Data into each year 
+  NEI99 <- subset(NEI, year==1999)
+  NEI02 <- subset(NEI, year==2002)  
+  NEI05 <- subset(NEI, year==2005)
+  NEI08 <- subset(NEI, year==2008)
+  
+  #getting total emissions for each year
+  totalPM25In99 <- sum(NEI99$Emissions)
+  totalPM25In02 <- sum(NEI02$Emissions)
+  totalPM25In05 <- sum(NEI05$Emissions)
+  totalPM25In08 <- sum(NEI08$Emissions)
+
+  myYearsVector <- c(1999,2002,2005,2008)
+  myEmissionsVector <- c(totalPM25In99, totalPM25In02, totalPM25In05, totalPM25In08)
+  
+  png('plo1.png')
+  plot(myYearsVector, myEmissionsVector, xlab="Year", ylab="Total Emissions", pch=20, main="Total PM25 Emissions From All Sources by Year")
+  dev.off()
+  #View(myTotalEmissionsByYear)
+  
+}
